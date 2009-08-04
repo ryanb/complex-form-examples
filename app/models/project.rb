54f994dfc3cafb781d9bf578934dabeb0f0c4d5b
Ryan Bates <ryan@railscasts.com>
2009-08-04 16:20:18 -0700

    adding destroy dependency for has_many association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
   validates_presence_of :name
   accepts_nested_attributes_for :tasks, :reject_if => lambda { |a| a.values.all?(&:blank?) }, :allow_destroy => true
 end
