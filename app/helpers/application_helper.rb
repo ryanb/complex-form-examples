fe80fa8cc39673d24f14f121e50483bc528a1ec9
Antonio Bardazzi <antonio.bardazzi@gmail.com>
2010-12-02 23:09:18 +0100

    to rails-2-3-stable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def remove_child_link(name, f)
-    f.hidden_field(:_delete) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
   
   def add_child_link(name, f, method)
